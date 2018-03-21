f7afb3f2f61a45844b29d79bd09a7848e7372ef2
Ben Sharafian <benms64@gmail.com>
2018-03-21 15:48:51 -0700

    fix: spsp host

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,9 +8,9 @@ class Config {
     this.plugin = IlpPlugin()
     this.dbPath = process.env.SPSP_DB_PATH
     this.token = process.env.SPSP_AUTH_TOKEN || 'test'
-    this.host = process.env.SPSP_HOST || this.localtunnel
+    this.host = process.env.SPSP_HOST || (this.localtunnel
       ? this.subdomain + '.localtunnel.me'
-      : 'localhost:' + this.port
+      : 'localhost:' + this.port)
 
     if (this.localtunnel && !this.subdomain) {
       throw new Error('subdomain must be specified if localtunnel is used')
